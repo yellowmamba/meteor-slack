f4f5349c53dc538e179be477258d40b3c0bad097
Michael  Wu <wujianfei871015@gmail.com>
2015-04-03 15:27:02 +1100

    Get the basics working

--- meteor-slack.js
+++ meteor-slack.js
@@ -1,4 +1,27 @@
+Messages = new Mongo.Collection("messages");
+
 if (Meteor.isClient) {
+	Template.body.helpers({
+		messages: function () {
+			return Messages.find({});
+		}
+	});
+
+	Template.body.events({
+		"submit .new-message": function (event) {
+			var newMessage = event.target.new_message.value;
+
+			Messages.insert({
+				message: newMessage,
+				createdAt: new Date(),
+				sender: 'Undefined',
+			});
+
+			event.target.new_message.value = '';
+
+			return false;
+		}
+	});
 }
 
 if (Meteor.isServer) {
